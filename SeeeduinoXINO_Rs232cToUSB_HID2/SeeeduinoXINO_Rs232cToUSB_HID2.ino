7c2cc86427af2825e48815e09a863cd9a41f3bf7
Ken Matsuoka <46617422+km190112@users.noreply.github.com>
2022-09-13 23:10:21 +0900

    Add files via upload

--- SeeeduinoXINO_Rs232cToUSB_HID2/SeeeduinoXINO_Rs232cToUSB_HID2.ino
+++ SeeeduinoXINO_Rs232cToUSB_HID2/SeeeduinoXINO_Rs232cToUSB_HID2.ino
@@ -1,26 +1,29 @@
 /*
-  Seeeduino XINO USB HID
-  ハードウェアシリアル
-  UART0 USB
-  UART1（RX：GPIO7、TX：GPIO6）機器側TXのみ使用
+  RS323C to TTLコンバータと接続し、設備から出力した信号をPCにHID入力する。
+  連続で改行コードを出力するような機器との接続する場合HIDの出力速度より入力速度を上回る。
+  ハードウェアシリアル64byteを超える可能性があため、一旦char型の２次元配列に格納し、
+  のバッファ溢れを防止する。
+
+  接続：Seeedino XINO
+  ハードウェアシリアル 
+  UART1（RX：GPIO7、TX：GPIO6）
+  RX：GPIO7 -- TX
+  TX：GPIO6 -- RX
+
+  機器側通信設定
+  ボーレート：9600
+  ビット長：8bit、パリティ：なし、ストップビット：なし(SERIAL_8N1)
 
   ライブラリ(adafruit/Adafruit_TinyUSB_Arduino)
   https://github.com/adafruit/Adafruit_TinyUSB_Arduino
-
-  機器側設定
-  ボーレート：9600
-  ビット長：8ビット、パリティ：なし、ストップビット：なし(SERIAL_8N1)
-
-  非同期シリアル通信(UART)で受信したデータをUSBでデータ送信。
-  USB HIDデバイスとして動かすことが出来る。
-
 */
+
 #include <Keyboard.h>
 
-#define BPS0 9600                 // bps UART0(USB)
+#define BPS0 9600                 // bps UART0(USB)test用
 #define BPS1 9600                 // bps UART1
 #define BAFFSIZE 150              // 連続で受信する場合の文字列のバッファ数
-#define DATASIZE 80               // 一度に送る文字列のバイト数
+#define DATASIZE 80               // 一度に送る文字列のバイト数のヴバッファ数
 
 char baffArr[BAFFSIZE][DATASIZE]; // バッファ
 int ReadX = 0;
@@ -28,9 +31,8 @@ int ReadY = 0;
 int WriteX = 0;
 int WriteY = 0;
 
-#define INTERVAL 900   // 最小送信間隔ms(アプリの入力速度に合わせて変更する)
+#define INTERVAL 800   // 最小送信間隔ms(アプリの入力速度に合わせて変更する)
 int count = 0;
-String strDATA = "";   // 送信する文字列
 
 /***************************************************************
    初期化
@@ -38,9 +40,9 @@ String strDATA = "";   // 送信する文字列
 void setup() {
   strDATA.reserve(DATASIZE); // strDATAの文字列の格納領域をbyte数確保
   Keyboard.begin();          // USB HID接続開始
-  Serial.begin(BPS0);        // UART0
+  // Serial.begin(BPS0);        // UART0
   Serial1.begin(BPS1);       // UART1
-  delay(500);
+  delay(50);
   //Keyboard.println("USB HID Start!!");
 }
 
@@ -65,18 +67,16 @@ void loop() {
   //書き出し
   if (INTERVAL <= count) {
     if (ReadX != WriteX) {             //データが保存されている場合に実行
+
       for (WriteY = 0; WriteY < DATASIZE; WriteY++) {
         if (baffArr[WriteX][WriteY] != '\0') {
-          strDATA += baffArr[WriteX][WriteY];
+          Keyboard.println(baffArr[WriteX][WriteY]);     // データ送信
           baffArr[WriteX][WriteY] = '\0';
         } else {
           break;
         }
       }
-      if (strDATA.length() > 1) {
-        Keyboard.println(strDATA);     // データ送信
-      }
-      strDATA = "";                    // データ初期化
+      
       WriteX += 1;
       WriteX = WriteX % BAFFSIZE;
     }

0408fc3f8ce6008a05425498c2fbce5c5a3d0a15
Ken Matsuoka <46617422+km190112@users.noreply.github.com>
2023-07-26 14:13:17 +0900

    Add files via upload

---
+++ Rs232cToUSB_HID2/Rs232cToUSB_HID2.ino
@@ -0,0 +1,97 @@
+/*
+  RS323C to TTLコンバータと接続し、設備から出力した信号をPCにHID入力する。
+  連続で多くのデータを出力するような機器との接続する場合HIDの出力速度より入力速度を上回るため、
+  改行コードごとにdelay処理を加えている。
+  またマイコンのハードウェアシリアルのバッファが64byteを超える可能性があため、
+  一旦char型の２次元配列に改行コードごとに格納し、バッファ溢れを防止する。
+
+  接続：Seeedino XINO
+  ハードウェアシリアル 
+  UART1（RX：GPIO7、TX：GPIO6）
+  RX：GPIO7 -- TX
+  TX：GPIO6 -- RX
+
+  機器側通信設定
+  ボーレート：9600
+  ビット長：8bit、パリティ：なし、ストップビット：なし(SERIAL_8N1)
+
+  ライブラリ(adafruit/Adafruit_TinyUSB_Arduino)
+  https://github.com/adafruit/Adafruit_TinyUSB_Arduino
+*/
+
+#include <Keyboard.h>
+
+#define BPS0 9600                 // bps UART0(USB)test用
+#define BPS1 9600                 // bps UART1
+#define BAFFSIZE 130              // 連続で受信する場合の文字列のバッファ数
+#define DATASIZE 80               // 一度に送る文字列のバイト数のバッファ数
+
+char baffArr[BAFFSIZE][DATASIZE]; // バッファ
+int ReadX = 0;
+int ReadY = 0;
+int WriteX = 0;
+int WriteY = 0;
+
+#define INTERVAL 800   // 最小送信間隔ms(アプリの入力速度に合わせて変更する)
+int count = 0;
+
+/***************************************************************
+   初期化
+ **************************************************************/
+void setup() {
+  Keyboard.begin();          // USB HID接続開始
+  // Serial.begin(BPS0);        // UART0
+  Serial1.begin(BPS1);       // UART1
+  delay(50);
+  //Keyboard.println("USB HID Start!!");
+}
+
+
+/***************************************************************
+  1バイトづつSerialで読み込み、baffArrに一旦格納する。strDATAに結合してUSB HIDで送信
+   *************************************************************/
+void loop() {
+  //読み込み
+  while (Serial1.available() > 0) {     // 受信したデータバッファが1バイト以上存在する場合
+    char inChar = (char)Serial1.read(); // Serial1からデータ読み込み
+    if (inChar == '\n') {               // 改行(LF:0x0a)がある場合の処理
+      baffArr[ReadX][ReadY] = '\0';
+      ReadX += 1;
+      ReadX = ReadX % BAFFSIZE;
+      ReadY = 0;
+
+    } else if (inChar != '\r') {       // 復帰(CR:0x0d)の場合は結合しない
+      
+      baffArr[ReadX][ReadY] = inChar;  // 読み込んだデータを結合
+      ReadY += 1;
+    }
+
+    if(ReadY >= DATASIZE){
+      for (WriteY = 0; WriteY < DATASIZE; WriteY++) {
+        baffArr[WriteX][WriteY] = '\0';
+        ReadY = 0;
+      }
+    }
+  }
+
+  //書き出し
+  if (INTERVAL <= count) {
+    if (ReadX != WriteX) {             //データが保存されている場合に実行
+
+      for (WriteY = 0; WriteY < DATASIZE; WriteY++) {
+        if (baffArr[WriteX][WriteY] != '\0') {
+          Keyboard.println(baffArr[WriteX][WriteY]);  // データ送信
+          baffArr[WriteX][WriteY] = '\0';
+        } else {
+          break;
+        }
+      }
+      WriteX += 1;
+      WriteX = WriteX % BAFFSIZE;
+    }
+  }
+
+  count = count % INTERVAL;
+  count += 1;
+  delay(1);
+}
